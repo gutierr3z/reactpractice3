803f5700481763beed538f0f72121f5fb6d2cf41
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-04 19:14:26 -0400

    following template

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
-console.log('Hello World!');
-
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
+class APP extends Component {
+
+    constructor( props ) {
+        super( props );
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return <div>hello</div>;
+    };
+}
+
+ReactDOM.render( <APP />, document.querySelector( '#main' ) );
