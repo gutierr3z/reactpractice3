d2150246793161ca26a1409670b37836dd8b821a
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-04 19:07:02 -0400

    following template

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 const express = require('express');
-
 const webpackDevMiddleware = require('webpack-dev-middleware');
-
 const webpack = require('webpack');
-
 const webpackConfig = require('./webpack.config.js');
-
 const app = express();
  
-
 const compiler = webpack(webpackConfig);
- 
 
 app.use(express.static( __dirname + '/www' ));
  
-
 app.use(webpackDevMiddleware(compiler, {
-
   hot: true,
   filename: 'bundle.js',
   publicPath: '/',
@@ -24,14 +16,11 @@ app.use(webpackDevMiddleware(compiler, {
     colors: true,
   },
   historyApiFallback: true,
-
 }));
  
 
 const server = app.listen(3000, function() {
-
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
-  
 });
