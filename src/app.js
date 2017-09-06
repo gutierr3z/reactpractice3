36cadd4870faa3bdfbb740055c03bd9d711a80e8
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-06 17:54:58 -0400

    following practice

--- src/app.js
+++ src/app.js
@@ -2,19 +2,38 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class APP extends Component {
-
     constructor( props ) {
-
         super( props );
-        
         this.state = {
-            item: ''
+
         };
     };
 
     render() {
-        return <div>yay!</div>
+        return <h1>yay!</h1>
     };
 };
 
 ReactDOM.render( <APP />, document.querySelector( '#main' ) );
+
+
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+
+// class APP extends Component {
+
+//     constructor( props ) {
+
+//         super( props );
+        
+//         this.state = {
+//             item: ''
+//         };
+//     };
+
+//     render() {
+//         return <div>yay!</div>
+//     };
+// };
+
+// ReactDOM.render( <APP />, document.querySelector( '#main' ) );
