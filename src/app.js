8e395f8242774cbeec25c02d8e02a16ddb66ca6a
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-08 18:28:06 -0400

    following template

--- src/app.js
+++ src/app.js
@@ -12,25 +12,32 @@ class APP extends Component {
 
     render() {
 
-        return <h1>hi hi hi hi</h1>;
+        return <h1>Calligraphy</h1>;
     };
 };
 
 ReactDOM.render( <APP />, document.querySelector( '#main' ) );
 
 
+
 // import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 
 // class APP extends Component {
+
 //     constructor( props ) {
+
 //         super( props );
+
 //         this.state = {};
 //     };
 
 //     render() {
-//         return <h1>hello there</h1>;
+
+//         return <h1>hi hi hi hi</h1>;
 //     };
 // };
 
 // ReactDOM.render( <APP />, document.querySelector( '#main' ) );
+
+
