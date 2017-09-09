7ed64effa82755a5613d8adb719cd486882b38bc
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-09 18:24:00 -0400

    following template

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+//- - - - - - - - - - - - - - - - - - - - - - - - - 
 import SubPage from './Components/sub1';
 import SubPage2 from './Components/sub2';
-//--------------------------------------------------
+//==================================================
 class APP extends Component {
 
-    constructor( props ) {
-        super( props );
+    constructor(props) {
+        super(props);
         this.state = {
-            list: [ 'un', 'deux', 'trois' ],
+            list: ['un', 'deux', 'trois'],
             theTxt: 'ssss'
         };
     };
@@ -17,13 +17,12 @@ class APP extends Component {
     render() {
         return (
             <div>
-                <h1>LEVEL ONE : { this.state.list[0] }</h1>
-                <div id="test">X</div>
-                <SubPage proList = { this.state.list } />
+                <h1>LEVEL ONE : {this.state.list[0]}</h1>
+                <SubPage proList={this.state.list} />
                 <SubPage2 />
             </div>
         );
     };
 };
-//--------------------------------------------------
-ReactDOM.render( <APP />, document.querySelector( '#main' ) );
+//==================================================
+ReactDOM.render(<APP />, document.querySelector('#main'));
