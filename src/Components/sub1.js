1cc5b30fadd4229f57b866b65fd44e5ab550e7e6
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-09 11:57:34 -0400

    following template

--- src/Components/sub1.js
+++ src/Components/sub1.js
@@ -7,27 +7,11 @@ const SubPage = ( props ) => {
     return (
         <div>
             <h2>LEVEL TWO : { props.theList[1] } </h2>
+            <input type="text" id="ipt_one" />
+            <input id="btn_one" type="button" value="Press" />
             <SubSubPage XXX = { props.theList } />
         </div>
     );
 };
 
 export default SubPage;
-
-
-
-// import React from 'react';
-// import SubSubPage from './sub1a';
-
-// const SubPage = ( props ) => {
-
-//     return (
-//         <div className="level2">
-//             <h2>SUBPAGE { props.list[1] }</h2>
-//             <SubSubPage selectedItem = { props.list[0] } />
-//         </div>
-//     );
-// };
-
-
-// export default SubPage;
