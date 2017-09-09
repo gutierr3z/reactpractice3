ec8568c3082efdab8d9be86735b0063a6dda268e
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-08 20:17:20 -0400

    following template

--- src/Components/sub1a.js
+++ src/Components/sub1a.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const SubSubPage = ( props ) => {
 
-    return <h3>SUB SUB PAGE { props.selectedItem }</h3>;
+    return (
+        <h3>SUB SUB PAGE { props.selectedItem }</h3>
+    );
 };
 
 export default SubSubPage;
